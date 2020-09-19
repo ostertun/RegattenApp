7e8d863e265379da8e394a2c46a50fe4b04c3d0a
Timon Ostertun <dev@ostertun.net>
2020-09-19 12:59:09 +0200

    Added temporary index

---
+++ content/index.php
@@ -0,0 +1,22 @@
+<?php
+	
+	$sp['output'] = '<div class="card bg-transparent" data-card-height="cover">
+            <div class="card-center text-center">
+                <i class="fa fa-exclamation-triangle fa-8x color-red2-dark"></i>
+                <h4 class="mb-5 mt-3">Startseite</h4>
+                <p>
+					Diese Seite befindet sich noch in der Entwicklung.
+                </p>
+
+                <div class="row ml-5 mr-5 mt-5 mb-0">
+                    <div class="col-6">
+                        <a href="' . LINK_PRE . 'index" class="btn btn-m rounded-sm btn-full bg-red2-dark text-uppercase font-900">Zur Startseite</a>
+                    </div>
+                    <div class="col-6">
+                        <a href="' . LINK_PRE . 'contact" class="btn btn-m rounded-sm btn-full bg-red2-dark text-uppercase font-900">Kontakt</a>
+                    </div>
+                </div>
+            </div>
+        </div>';
+	
+?>
