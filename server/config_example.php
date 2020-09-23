04a3348db7e0149424f73990117af2be05f3204d
Timon Ostertun <dev@ostertun.net>
2020-09-23 23:41:47 +0200

    Removed app icons from git

--- server/config_example.php
+++ server/config_example.php
@@ -5,7 +5,6 @@
 	define('SEND_ERRORS', true);   // send errors via log
 	
 	date_default_timezone_set('Europe/Berlin');
-	define('SERVER_GITDIR', __DIR__ . '/../.git/');  // path to git folder (with trailing slash)
 	define('SERVER_PATH', '/subfolder');  // path to root directory
 	define('SERVER_ADDR', 'https://' . $_SERVER['SERVER_NAME'] . SERVER_PATH);  // path to root directory
 	define('LOGGING_APIKEY', 'xxx');  // Apikey for Logging API -> get from ostertun.net/logging
